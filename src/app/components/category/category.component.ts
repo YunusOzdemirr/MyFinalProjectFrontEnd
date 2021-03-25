2ca53d1e8c2a69a23fc9e65ba371d8cf2800cab7
Yunus <yunusozdemir468@gmail.com>
2021-03-25 22:02:42 +0300

    Select the All Products operation

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -10,26 +10,45 @@ import { CategoryService } from 'src/app/services/category.service';
 export class CategoryComponent implements OnInit {
   categories: Category[] = [];
   currentCategory: Category;
-
   constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
     this.getCategories();
   }
+
   getCategories() {
     this.categoryService.getCategories().subscribe((response) => {
       this.categories = response.data;
     });
   }
+
   setCurrentCategory(category: Category) {
     this.currentCategory = category;
   }
+
   getCurrentCategoryClass(category: Category){
     if(category==this.currentCategory){
       return "list-group-item active"
-    }else
+    }
+    else
     {
       return "list-group-item"
     }
   }
+  getAllCategoryClass(){
+    if (!this.currentCategory || this.currentCategory.categoryName=="") {
+      return "list-group-item active"
+    }
+    else{
+      return "list-group-item"
+    }
+  }
+
+  clearCategory(){
+    this.currentCategory={
+      categoryId:-1,
+      categoryName:"",
+    }
+  }
+
 }
