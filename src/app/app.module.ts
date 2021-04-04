db9aaf649281b8cb3ec8973be9306ec9f0454a90
Yunus <yunusozdemir468@gmail.com>
2021-04-04 22:00:54 +0300

    Animations and filterText

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule } from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,8 @@ import { CategoryComponent } from './components/category/category.component';
 import { NaviComponent } from './components/navi/navi.component';
 import { VatAddedPipe } from './pipes/vat-added.pipe';
 import { FilterPipePipe } from './pipes/filter-pipe.pipe';
+
+import { ToastrModule } from 'ngx-toastr';
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +20,7 @@ import { FilterPipePipe } from './pipes/filter-pipe.pipe';
     CategoryComponent,
     NaviComponent,
     VatAddedPipe,
-    FilterPipePipe
+    FilterPipePipe,
   ],
   //Bizim oluşturduklarımızı declarations'a yazıyoruz
   //Dışardan gelenleri importa
@@ -26,8 +29,12 @@ import { FilterPipePipe } from './pipes/filter-pipe.pipe';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+    }),
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -11,33 +12,23 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-  // productResponseModel: ProductResponseModel={
-  //   data:this.products,
-  //   message:"",
-  //   success:true
-  // };
   products: Product[] = [];
   dataLoaded = false;
-  filterText="";
+  filterText = '';
   constructor(
     private productService: ProductService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private toastrService: ToastrService
   ) {}
   //ActivatedRoute dışarıdan gelen bir parametreyi almak için kullanılıyor.
   ngOnInit(): void {
-    // console.log('Init çalıştı');
-    // this.getProducts();
-    // .subscribe((response) => {
-    //   this.products = response.data;
-    // });
-    this.activatedRoute.params.subscribe(params => {
-      if (params["categoryId"]){
-        this.getProductsByCategoryId(params["categoryId"]);
-      }else
-      {
+    this.activatedRoute.params.subscribe((params) => {
+      if (params['categoryId']) {
+        this.getProductsByCategoryId(params['categoryId']);
+      } else {
         this.getProducts();
       }
-    })
+    });
   }
 
   getProducts() {
@@ -46,11 +37,15 @@ export class ProductComponent implements OnInit {
       this.dataLoaded = true;
     });
   }
-  getProductsByCategoryId(categoryId:number) {
-    this.productService.getProductsByCategoryId(categoryId).subscribe((response) => {
-      this.products = response.data;
-      this.dataLoaded = true;
-    });
+  getProductsByCategoryId(categoryId: number) {
+    this.productService
+      .getProductsByCategoryId(categoryId)
+      .subscribe((response) => {
+        this.products = response.data;
+        this.dataLoaded = true;
+      });
   }
-
+  addToCart(product: Product) {
+    this.toastrService.success("Sepete eklendi",product.productName);
+  }
 }
